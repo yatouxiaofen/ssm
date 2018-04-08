0b524df4f93a7bdf052fd434b1f240699612fcbe
yatouxiaofen <liusufenaij@163.com>
2018-04-08 17:01:30 +0800

    修改日志格式  添加登录用户名

--- src/main/java/com/susu/ssm/controller/IndexController.java
+++ src/main/java/com/susu/ssm/controller/IndexController.java
@@ -1,14 +1,35 @@
 package com.susu.ssm.controller;
 
+import common.framework.bean.ResultBean;
+import common.framework.utils.UserUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/")
 public class IndexController
 {
+    private static final Logger logger = LoggerFactory.getLogger(IndexController.class);
+
     @RequestMapping("")
     public String index(){
         return "admin/index";
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    @ResponseBody
+    public ResultBean<String> login(HttpSession session, String username) {
+        logger.info("login user:" + username);
+
+        // TODO 只是模拟登陆
+        session.setAttribute(UserUtil.KEY_USER, username);
+
+        return new ResultBean<String>(username);
     }
 }
--- src/main/java/com/susu/ssm/service/impl/UserServiceImpl.java
+++ src/main/java/com/susu/ssm/service/impl/UserServiceImpl.java
@@ -2,6 +2,8 @@ package com.susu.ssm.service.impl;
 
 import javax.annotation.Resource;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import com.susu.ssm.dao.UserMapper;
@@ -13,17 +15,25 @@ import java.util.List;
 @Service("userService")
 public class UserServiceImpl implements UserService
 {
+    private static final Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
+
     @Resource
     private UserMapper userMapper;
 
     @Override
     public User getUserById(int userId)
     {
-        return this.userMapper.selectByPrimaryKey(userId);
+        logger.info("getUserById start,userId="+userId);
+        User user = this.userMapper.selectByPrimaryKey(userId);
+        logger.info("getUserById end, user info", user.toString());
+        return user;
     }
 
     @Override
     public List<User> getUserList() {
-        return this.userMapper.selectUserList();
+        logger.info("getUserList start ...");
+        List<User> userList = this.userMapper.selectUserList();
+        logger.info("getUserList end, data size:{}", userList.size());
+        return userList;
     }
 }
