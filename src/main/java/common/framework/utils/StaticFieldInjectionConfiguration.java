636c8d30d575a5ded3869c4567483bebc87c10c9
yatouxiaofen <liusufenaij@163.com>
2018-04-08 14:49:08 +0800

    基础工具类

---
+++ src/main/java/common/framework/utils/StaticFieldInjectionConfiguration.java
@@ -0,0 +1,26 @@
+package common.framework.utils;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Component;
+
+/**
+ * 工具类的注入
+ * 
+ * @author 肖文杰 https://xwjie.github.io/PLMCodeTemplate/
+ *
+ */
+@Component
+public class StaticFieldInjectionConfiguration {
+
+    @Autowired
+    MessageSource resources;
+
+    @PostConstruct
+    private void init() {
+    	System.out.println("\n\n-----StaticFieldInjectionConfiguration----");
+		CheckUtil.setResources(resources);
+    }
+}
